1ed7d054fc50cd7c49e435c1134e2249b5e87e06
gabefinch <gabefinch@gmail.com>
2016-05-08 20:45:54 -0700

    animate header

--- source/assets/javascripts/application.js
+++ source/assets/javascripts/application.js
@@ -1,1 +1,10 @@
 //= require_tree .
+
+$(window).load(function(){
+  if(Math.random() > 0.5) {
+    $('#header').empty().html('Allisa <span class="amp">&amp;</span> Gabe');
+  } else {
+    $('#header').empty().html('Gabe <span class="amp">&amp;</span> Allisa');
+  }
+  $('#header').animate({opacity: 1}, 2000);
+});
