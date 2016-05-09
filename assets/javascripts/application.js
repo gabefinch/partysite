1601b5c2258fdcfa9cd0e2b8ed5369a4bab891e6
gabefinch <gabefinch@gmail.com>
2016-05-08 20:46:26 -0700

    Automated commit at 2016-05-09 03:46:26 UTC by middleman-deploy 2.0.0-alpha

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -27,3 +27,12 @@ $(window).on("load resize",function(e) {
     });
   });
 });
+
+$(window).load(function(){
+  if(Math.random() > 0.5) {
+    $('#header').empty().html('Allisa <span class="amp">&amp;</span> Gabe');
+  } else {
+    $('#header').empty().html('Gabe <span class="amp">&amp;</span> Allisa');
+  }
+  $('#header').animate({opacity: 1}, 2000);
+});
